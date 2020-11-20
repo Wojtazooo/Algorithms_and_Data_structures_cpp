95adf98899e0357e1592f67abadf1d2384345a07
Wojtazooo <wojtekulaski@gmail.com>
2020-11-20 20:15:43 +0100

    add DelMax

--- Linked_List/main.cpp
+++ Linked_List/main.cpp
@@ -19,7 +19,11 @@ public:
 		elem* temp = head;
 		while (temp != nullptr)
 		{
-			std::cout << temp->val << " ";
+			std::cout << temp->val;
+			if (temp->next != nullptr)
+				std::cout << " -> ";
+			else
+				std::cout << "\n";
 			temp = temp->next;
 		}
 	}
@@ -30,13 +34,13 @@ public:
 		elem* e = new elem{v, head};
 		head = e;
 	}
-
-
+	
 	void Insert_Last(int v)
 	{
+		elem* to_add = new elem{ v, nullptr };
 		if (head == nullptr) // if list is empty
 		{
-			head = new elem{v, nullptr};
+			head = to_add;
 			return;
 		}
 		// iterate to end of list
@@ -45,15 +49,51 @@ public:
 		{
 			p = p->next;
 		}
-		p->next = new elem{ v,nullptr };
+		p->next = to_add;
+	}
+
+	elem* DelMax()
+	{
+		elem *max = head; // pointer to max elem
+		elem* smax = nullptr; // shadow of max elem
+		elem* temp = head; 
+		elem* stemp = nullptr; // shadow of temp
+		while (temp)
+		{
+			if (temp->val > max->val)
+			{
+				smax = stemp;
+				max = temp;
+			}
+			stemp = temp;
+			temp = temp->next;
+		}
+		if (max != nullptr) // if list is not empty
+		{
+			if (smax != nullptr) // if max is not first elem in list
+				smax->next = max->next;
+			else // max is at the beginning
+				head = max->next;
+		}
+		return max;
 	}
 };
 
 int main()
 {
 	LinkedList L;
-	L.Insert(1);
-	L.Insert_Last(2);
-	L.Insert(3);
+	L.Insert_Last(1);
+	L.Insert(2);
+	L.Insert_Last(30);
+	L.Insert(4);
+	L.Insert(5);
+	L.Insert_Last(6);
+	L.Insert(50);
+	L.PrintAll();
+	std::cout << "Delmax\n";
+	L.DelMax();
+	L.PrintAll();
+	std::cout << "Delmax\n";
+	L.DelMax();
 	L.PrintAll();
 }
