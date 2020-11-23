156e3800e0353211449b2e919d6fca4b954e1c23
Wojtazooo <wojtekulaski@gmail.com>
2020-11-23 21:56:53 +0100

    add tests

--- main.cpp
+++ main.cpp
@@ -1,37 +1,36 @@
-//// LinkedList usage
-//#include "LinkedList/Linkedlist.h"
-//int main()
-//{
-//	LinkedList L;
-//	L.Insert_Last(1);
-//	L.Insert(2);
-//	L.Insert_Last(30);
-//	L.Insert(4);
-//	L.Insert(5);
-//	L.Insert_Last(6);
-//	L.Insert(50);
-//	L.PrintAll();
-//	std::cout << "Delmax\n";
-//	L.DelMax();
-//	L.PrintAll();
-//	std::cout << "Delmax\n";
-//	L.DelMax();
-//	L.PrintAll();
-//}
-
-
-// LinkedList usage
+#include "Linkedlist.h"
 #include "OrderedLinkedList.h"
 int main()
 {
-	OrderedLinkedList OL;
-	OL.Insert(2);
-	OL.Insert(3);
-	OL.Insert(1);
-	OL.Insert(5);
-	OL.Insert(2);
-	OL.PrintAll();
-	std::cout << "Delmax\n";
-	OL.DelMax();
-	OL.PrintAll();
+	// LinkedList test
+	{
+		std::cout << "==================\n";
+		std::cout << "Linked list\n";
+		LinkedList L;
+		L.Insert(1); L.Insert(2); L.Insert_Last(5); L.Insert(4);
+		L.PrintAll();
+		std::cout << "Delmax\n";
+		L.DelMax();
+		L.PrintAll();
+		std::cout << "==================\n";
+
+	}
+
+	// OrderedLinkedList test
+	{
+		std::cout << "Ordered linked list\n";
+		OrderedLinkedList OL;
+		OL.Insert(2);
+		OL.Insert(3);
+		OL.Insert(1);
+		OL.Insert(5);
+		OL.Insert(2);
+		OL.PrintAll();
+		std::cout << "Delmax\n";
+		OL.DelMax();
+		OL.PrintAll();
+		std::cout << "==================\n";
+	}
+
+	
 }
